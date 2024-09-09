e60be6c29369b9fa07dc839b49cd4868cb4f4bba
LeroySBN <leroybuliro@gmail.com>
2024-09-09 15:58:02 +0300

    restructuring project

--- services/files_manager/tests/init.test.js
+++
@@ -1,7 +0,0 @@
-import supertest from 'supertest';
-import chai from 'chai';
-import api from '../server';
-
-global.request = supertest(api);
-global.expect = chai.expect;
-global.assert = chai.assert;
